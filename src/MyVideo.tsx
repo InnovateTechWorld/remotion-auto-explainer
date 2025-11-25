795ae341fccea88f695dec4e5ab50b328890794e
Obimba Joshua <obimz123@gmail.com>
2025-11-25 10:16:41 +0100

    feat: add default logoPath prop to MyVideo component

--- src/MyVideo.tsx
+++ src/MyVideo.tsx
@@ -81,6 +81,7 @@ export const RemotionRoot: React.FC = () => {
         defaultProps={{
           script: [],
           audioPath: '',
+          logoPath: '',
           durationInFrames: 1
         }}
       />
